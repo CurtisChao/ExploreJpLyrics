8afd720ed397d56722d83a709c604f6a60716bd9
DESKTOP-J533UFS\17789 <cchao38@my.bcit.ca>
2026-07-15 23:35:26 -0700

    Add username password authentication provider

---
+++ ExploreJpLyrics_Backend/src/main/java/com/exploreJpLyrics/backend/security/DefaultAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.exploreJpLyrics.backend.security;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultAuthenticationProvider implements AuthenticationProvider {
+
+	private final UserDetailsService userDetailsService;
+	private final PasswordEncoder passwordEncoder;
+
+	public DefaultAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		this.userDetailsService = userDetailsService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String password = authentication.getCredentials().toString();
+
+		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+		if (passwordEncoder.matches(password, userDetails.getPassword())) {
+			return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+		} else {
+			throw new BadCredentialsException("BAD CREDENTIALS");
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
